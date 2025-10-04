47f132f463ea700c91afce289eab2f16113f728f
kassandra30 <albercakassandra@gmail.com>
2025-10-03 23:31:50 -0500

    Delete Ejercicio 04.py

--- Ejercicio 04.py
+++
@@ -1,9 +0,0 @@
-"""
-Programa que formatea un número decimal con diferentes precisiones
-"""
-numero = float(input("Ingrese un número decimal (con 6 decimales): "))
-
-# Mostrar con diferentes precisiones
-print(f"1 decimal: {numero:.1f}")
-print(f"2 decimales: {numero:.2f}")
-print(f"4 decimales: {numero:.4f}")
